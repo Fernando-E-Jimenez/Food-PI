d2d184248c8ecd3dbcdeb5ec0b9fa05e9151aa75
Ferve <ferve91@gmail.com>
2022-06-26 16:00:44 -0300

    Cambiando la bar de busquedas porque no me estaba trayendo todo lo necesario

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { useDispatch } from "react-redux";
-import { searchByName, getRecipes } from "../actions";
+import { searchByName } from "../actions";
 import styles from "./SearchBar.module.css"
 
 export default function SearchBar({returnToFirstPage}){
@@ -9,22 +9,17 @@ export default function SearchBar({returnToFirstPage}){
     
     function handleChange(e){
         e.preventDefault();
-        if (e.target.value) {
             setName(e.target.value);
-        }else {
-            dispatch(getRecipes)
-        }
     };
     function handleButton(e){
         e.preventDefault();
         dispatch(searchByName(name))
-        setName("")
         .then(() =>{ returnToFirstPage(); })        
     };
 
     return (
         <div className={styles.searchContainer}>
-            <input className={styles.searchInput} type="text" value={name}
+            <input className={styles.searchInput} type="text"
             placeholder='Search:' 
             onChange={(e) => handleChange(e)}/>
             <button className={styles.buttonSearch} type='submit' onClick={ (e) => handleButton(e)} >Search</button>
